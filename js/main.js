7272b02a567e9308ea6a6ca75a15e8d3a9230197
VitaliiPopov00 <112977103+VitaliiPopov00@users.noreply.github.com>
2023-09-26 21:30:47 +0300

    review code and fix bugs

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     //     $('.product_img').toggleClass('no-show');
     // });
 
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
         $('.main_screen').css('height', `${$(window).height() - heightHeader}px`);
         $('.nav_small_screen').css('height', `${$(window).height()}px`);
     });
@@ -29,23 +29,6 @@ $(document).ready(function () {
 
         if ($(this).hasClass('basket')) {
             $(this).html('В КОРЗИНЕ');
-
-            $(this).parent().find('.operation').find('.plus').on('click', function () {
-                let count = Number($(this).parent().find('.number').html());
-                $(this).parent().find('.number').html(count + 1);
-            });
-
-            $(this).parent().find('.operation').find('.minus').on('click', function () {
-                let count = Number($(this).parent().find('.number').html());
-
-                if (count == 1) {
-                    $(this).parent().toggleClass('no-show quantity_operation');
-                    $(this).parent().parent().find('.card_button').html('КУПИТЬ').toggleClass('basket');
-                    $(this).parent().find('.number').html('1');
-                }
-
-                $(this).parent().find('.number').html(count - 1);
-            });
         } else {
             $(this).parent().find('.operation').find('.number').html('1');
             $(this).html('КУПИТЬ');
@@ -56,31 +39,36 @@ $(document).ready(function () {
         e.preventDefault();
         $(this).toggleClass('basket');
         $(this).parent().find('.operation').toggleClass('no-show product_count');
-        
+
         if ($(this).hasClass('basket')) {
             $(this).html('В КОРЗИНЕ');
-
-            $(this).parent().find('.operation').find('.plus').on('click', function () {
-                e.preventDefault();
-                let count = Number($(this).parent().find('.number').html());
-                $(this).parent().find('.number').html(count + 1);
-            });
-
-            $(this).parent().find('.operation').find('.minus').on('click', function () {
-                e.preventDefault();
-                let count = Number($(this).parent().find('.number').html());
-
-                if (count == 1) {
-                    $(this).parent().toggleClass('no-show product_count');
-                    $(this).parent().parent().find('.product_buy').html('КУПИТЬ').toggleClass('basket');
-                    $(this).parent().find('.number').html('1');
-                }
-
-                $(this).parent().find('.number').html(count - 1);
-            });
         } else {
             $(this).parent().find('.operation').find('.number').html('1');
             $(this).html('КУПИТЬ');
+        }
+    })
+
+    $('.plus').on('click', function (e) {
+        e.preventDefault();
+        let count = Number($(this).parent().find('.number').html());
+        $(this).parent().find('.number').html(count + 1);
+    });
+
+    $('.minus').on('click', function (e) {
+        e.preventDefault();
+        let count = Number($(this).parent().find('.number').html());
+
+        if (count == 1) {
+            if ($(this).parent().hasClass('quantity_operation')) {
+                $(this).parent().toggleClass('no-show quantity_operation');
+                $(this).parent().parent().find('.card_button').html('КУПИТЬ').toggleClass('basket');
+            } else {
+                $(this).parent().toggleClass('no-show product_count');
+                $(this).parent().parent().find('.product_buy').html('КУПИТЬ').toggleClass('basket');
+            }
+            $(this).parent().find('.number').html('1');
+        } else {
+            $(this).parent().find('.number').html(count - 1);
         }
     });
 
