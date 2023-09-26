b1ddec0d71a7e36abd2e4e8e06face985284ff29
VitaliiPopov00 <112977103+VitaliiPopov00@users.noreply.github.com>
2023-09-26 15:49:49 +0300

    update product page

--- js/main.js
+++ js/main.js
@@ -52,6 +52,36 @@ $(document).ready(function () {
         }
     });
 
+    $('.product_buy').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('basket');
+        $(this).parent().find('.operation').toggleClass('no-show product_count');
+        
+        if ($(this).hasClass('basket')) {
+            $(this).html('В КОРЗИНЕ');
+
+            $(this).parent().find('.operation').find('.plus').on('click', function () {
+                let count = Number($(this).parent().find('.number').html());
+                $(this).parent().find('.number').html(count + 1);
+            });
+
+            $(this).parent().find('.operation').find('.minus').on('click', function () {
+                let count = Number($(this).parent().find('.number').html());
+
+                if (count == 1) {
+                    $(this).parent().toggleClass('no-show product_count');
+                    $(this).parent().parent().find('.product_buy').html('КУПИТЬ').toggleClass('basket');
+                    $(this).parent().find('.number').html('1');
+                }
+
+                $(this).parent().find('.number').html(count - 1);
+            });
+        } else {
+            $(this).parent().find('.operation').find('.number').html('1');
+            $(this).html('КУПИТЬ');
+        }
+    });
+
     $('.button_up').on('click', function (e) {
         e.preventDefault();
 
