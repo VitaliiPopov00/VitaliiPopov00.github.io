ae3504449f406b9ba615ce694bdcb255727111ea
VitaliiPopov00 <112977103+VitaliiPopov00@users.noreply.github.com>
2023-10-30 09:27:12 +0300

    add products page, global update all

---
+++ loader.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    setTimeout(() => {
+        $('.loader').fadeToggle(300, () => {
+            $('header, main, footer').animate({
+                opacity: 1
+            }, 300, 'swing', () => {
+                $('header, main, footer').removeClass('opacity');
+            });
+        });
+    }, 150);
+});
